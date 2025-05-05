6dce8b37056f0a02ae420f9edf5b809b6f402532
Bana0615 <1860512+Bana0615@users.noreply.github.com>
2025-05-05 15:03:21 +0000

    Prettier Snap! Formatting applied. ✨

--- app/drop-roulette/og/season-three/page.tsx
+++ app/drop-roulette/og/season-three/page.tsx
@@ -1,36 +1,36 @@
-import { Container, Row, Col } from "react-bootstrap";
-import type { Metadata } from "next";
+import { Container, Row, Col } from 'react-bootstrap';
+import type { Metadata } from 'next';
 // --- Layout ---
-import PageLayout from "@/components/PageLayout";
+import PageLayout from '@/components/PageLayout';
 // --- Components ---
-import DropRoulette from "@/components/DropRoulette";
+import DropRoulette from '@/components/DropRoulette';
 // --- Data ---
-import mapInfo from "@/json/drop-spot/og/season-one.json";
+import mapInfo from '@/json/drop-spot/og/season-one.json';
 
 export const metadata: Metadata = {
-  title: "Drop Roulette - Fortnite OG Season 3",
+  title: 'Drop Roulette - Fortnite OG Season 3',
   description:
-    "Spice up your Fortnite gameplay! Randomly roll where you should land in fortnite og season 3.",
+    'Spice up your Fortnite gameplay! Randomly roll where you should land in fortnite og season 3.',
 };
 
 export default function DropRouletteOgS03Page() {
   return (
     <PageLayout>
-      <Container className="mt-3 mb-3">
-        <Row className="shadow-lg p-3 bg-body rounded">
+      <Container className='mt-3 mb-3'>
+        <Row className='shadow-lg p-3 bg-body rounded'>
           <Col>
-            <h2 className="text-center my-3">
+            <h2 className='text-center my-3'>
               Drop Roulette
-              <span className="d-none d-sm-inline-block">&nbsp;-&nbsp;</span>
-              <br className="d-block d-sm-none" />
+              <span className='d-none d-sm-inline-block'>&nbsp;-&nbsp;</span>
+              <br className='d-block d-sm-none' />
               Fortnite OG Season 3
             </h2>
             <hr />
 
             <DropRoulette
-              map="OG Season 3"
-              button_key="br-og-3"
-              ga_label="BrOg-3"
+              map='OG Season 3'
+              button_key='br-og-3'
+              ga_label='BrOg-3'
               mapInfo={mapInfo}
             />
           </Col>
