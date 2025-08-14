c5538468a3d5e46012e8f8abbd9a647e4190e661
Bana0516 <aelbaneh0615@gmail.com>
2025-08-14 08:33:04 -0400

    Added Drop Roulette for chapter 6 season 4

--- src/app/drop-roulette/chapter-six/season-four/page.tsx
+++ src/app/drop-roulette/chapter-six/season-four/page.tsx
@@ -1,21 +1,23 @@
+// --- React ---
 import { Container, Row, Col } from 'react-bootstrap';
+// --- Next
 import type { Metadata } from 'next';
 // --- Layout ---
 import PageLayout from '@/components/PageLayout';
 // --- Components ---
 import DropRoulette from '@/components/drop-roulette/DropRoulette';
 // --- Data ---
-import mapInfo from '@/data/drop-spot/og/season-five.json';
+import mapInfo from '@/data/drop-spot/battle-royale/chapter-six/season-four.json';
 // --- Styles ---
 import styles from '@/components/drop-roulette/DropRoulette.module.css';
 
 export const metadata: Metadata = {
-  title: 'Drop Roulette - Fortnite OG Season 5',
+  title: 'Drop Roulette - Fortnite Chapter 6 Season 4',
   description:
-    'Drift into the action! Season 5 brings new worlds to explore with the arrival of the All-Terrain Kart and the mysterious Viking ship. Will you drop into the sandy paradise of Paradise Palms, hit the links at Lazy Links, or ride the rifts to victory?',
+    "It's a wild ride! The apocalypse is here, and the wasteland awaits. Use our randomizer to pick your landing spot in Fortnite Chapter 6 Season 4 and begin your post-apocalyptic adventure.",
 };
 
-export default function DropRouletteOgS03Page() {
+export default function DropRouletteCh06S01Page() {
   return (
     <PageLayout headerLogoUrl='/icons/logo.svg' headerLogoWidth={50} headerLogoHeight={50}>
       <Container className='mt-3 mb-3'>
@@ -25,14 +27,14 @@ export default function DropRouletteOgS03Page() {
               Drop Roulette
               <span className='d-none d-sm-inline-block'>&nbsp;-&nbsp;</span>
               <br className='d-block d-sm-none' />
-              Fortnite OG Season 5
+              Fortnite Chapter 6 Season 4
             </h1>
             <hr />
 
             <DropRoulette
-              map='OG Season 5'
-              button_key='br-og-5'
-              ga_label='BrOg-5'
+              map='Chapter 6 Season 4'
+              button_key='br-ch6-4'
+              ga_label='BrCh6-4'
               mapInfo={mapInfo}
             />
           </Col>
